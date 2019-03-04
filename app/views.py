6c79ab5eb75a1837a996e49bc1210c6406cb6c32
Eric Anderson <eric.anderson7@outlook.com>
2019-03-04 05:18:10 +0000

    Completed

--- app/views.py
+++ app/views.py
@@ -11,6 +11,10 @@ from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
 
+from werkzeug.security import check_password_hash
+from app.models import UserProfile
+from app.forms import LoginForm
+
 
 ###
 # Routing for your application.
@@ -31,10 +35,13 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        #if form.username.data:
+        username = form.username.data
+        password = form.password.data
+        
             # Get the username and password values from the form.
 
             # using your model, query database for a user based on the username
@@ -42,12 +49,20 @@ def login():
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
-
+        user = UserProfile.query.filter_by(username=username).first()
+            
+        if user is not None and (user.password, password):
+            
             # get user id, load into session
+            
             login_user(user)
 
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            flash('Logged in successfully.', 'success')
+            return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
+        else:
+               flash('Username or Password is incorrect.', 'danger')
+            #return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 
@@ -60,6 +75,14 @@ def load_user(id):
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+
+@app.route('/logout')
+@login_required
+def logout():
+    
+    logout_user()
+    flash('You have been logged out.', 'Bye!')
+    return redirect(url_for('home'))
 
 
 @app.route('/<file_name>.txt')
